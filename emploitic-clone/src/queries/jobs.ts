0ffd74d58753752e83ff0bf21f746860c68c1b55
ghada2025 <khenieneghada@gmail.com>
2025-03-13 00:07:59 +0100

    render

--- emploitic-clone/src/queries/jobs.ts
+++ emploitic-clone/src/queries/jobs.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export async function getJobs() {
-	const jobs = await axios.get("http://localhost:5000/jobs")
+	const jobs = await axios.get("https://jobs-api-ab9l.onrender.com/jobs")
 	return jobs.data
 }
